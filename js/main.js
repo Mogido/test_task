e9be0396a88e565c52f14b3cde6350be044c6fac
Maksim <makstrofi@yandex.ru>
2020-05-31 13:06:37 +0300

    finished desktop version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-    alert("подключился")
-
-    
+   'use strict'
+   
     const openPopupBtn = document.getElementById("open_button");
     const popupContent = document.querySelector(".main-content__order-popup");
     const closePopupBtn = document.getElementById("close_button");
+    const galleryBtn = document.getElementById("gallery-button");
+    const galleryContent = document.querySelector(".fd__feedback__gallery__container");
 
     openPopupBtn.addEventListener("click", function(event) {
         event.preventDefault();
@@ -13,7 +14,44 @@
     closePopupBtn.addEventListener("click", function(event) {
         event.preventDefault();
         popupContent.classList.remove("main-content__order-popup--open");
-    })
-    //slider
+    });
 
+    galleryBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        if (galleryContent.classList.contains("fd__feedback__gallery__container--open")) {
+            galleryContent.classList.remove("fd__feedback__gallery__container--open")
+        } else {
+        galleryContent.classList.add("fd__feedback__gallery__container--open");
+        $(document).ready(function(){
+            $('.fd__feedback__gallery').slick({
+                nextArrow: document.getElementById("arrow-next"),
+                prevArrow: document.getElementById("arrow-prev")
+            });
+          });};
+    });
+
+
+    $(".countdown-timer").TimeCircles(
+        { time: {
+            Days: { 
+                show: false,
+                 },
+            Hours: {
+                text: "",
+                color: "ffcb00"
+            },
+            Minutes: {
+                text: "",
+                color: "#ffcb00"
+            },
+            Seconds: {
+                text: "",
+                color: "#ffcb00"
+            }
+        },
+        circle_bg_color : "#ff1c00"}
+    );
+
+   
     
